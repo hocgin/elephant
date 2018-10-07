c7e290bbbffcd19b9a5cd6e3f5a319cd424b899e
hocgin <hocgin@gmail.com>
2018-10-08 00:52:30 +0800

    - .f

--- api/src/main/java/in/hocg/Application.java
+++ api/src/main/java/in/hocg/Application.java
@@ -10,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author hocgin
  */
 @SpringBootApplication
-public class ApiApplication {
+public class Application {
     
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
     
 }
