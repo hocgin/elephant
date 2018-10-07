8dd6bfaec56b8617801c0e2e53bb8da2ea0dd9e5
hocgin <hocgin@gmail.com>
2018-10-08 00:10:05 +0800

    Initial commit

---
+++ api/src/main/java/in/hocg/ApiApplication.java
@@ -0,0 +1,19 @@
+package in.hocg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2018/10/7.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class ApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+    
+}
