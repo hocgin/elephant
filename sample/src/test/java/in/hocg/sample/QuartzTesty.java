1bbdfe1e9225e29bed58f31775cefa23f9244908
hocgin <hocgin@gmail.com>
2018-10-12 11:49:35 +0800

    add Quartz JUnit

--- sample/src/test/java/in/hocg/sample/QuartzTesty.java
+++ sample/src/test/java/in/hocg/sample/QuartzTesty.java
@@ -1,6 +1,7 @@
 package in.hocg.sample;
 
 import in.hocg.job.TestJob;
+import in.hocg.sample.quartz.listener.TestJobListener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.quartz.*;
@@ -9,6 +10,8 @@ import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,34 +25,55 @@ public class QuartzTesty {
     @Test
     public void testX() throws InterruptedException, SchedulerException {
         System.out.println("==>");
-    
+        
         // 任务详情(执行内容)
         JobDetail detail = JobBuilder.newJob(TestJob.class)
-                .withIdentity("TestJob")
-                .storeDurably().build();
+                .withIdentity("TestJob", "g")
+                .storeDurably()
+                .build();
         
-        // 时间
-        SimpleScheduleBuilder simpleScheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+        // ＿簡單的时间
+        ScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
                 .withIntervalInSeconds(2)
                 .repeatForever();
         
+        // _Cron 表達式
+//        ScheduleBuilder cronSchedule = CronScheduleBuilder.cronSchedule("");
+        
         // 任务触发器
-        SimpleTrigger trigger = TriggerBuilder.newTrigger()
+        Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity("MyTrigger")
-                .withSchedule(simpleScheduleBuilder)
+                .withSchedule(scheduleBuilder)
                 .build();
         
+        // 執行器
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-        scheduler.scheduleJob(detail, trigger);
+        scheduler.getListenerManager()
+                .addJobListener(new TestJobListener());
         scheduler.start();
-    
-    
-        for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.anyGroup())) {
-            List<? extends Trigger> triggers = scheduler.getTriggersOfJob(jobKey);
-            System.out.println(triggers.size());
+        
+        
+        scheduler.scheduleJob(detail, trigger);
+        
+        
+        // 列出所有
+        for (String groupName : scheduler.getJobGroupNames()) {
+            
+            for (JobKey jobKey : scheduler.getJobKeys(GroupMatcher.jobGroupEquals(groupName))) {
+                
+                String jobName = jobKey.getName();
+                String jobGroup = jobKey.getGroup();
+                
+                //get job's trigger
+                List<Trigger> triggers = (List<Trigger>) scheduler.getTriggersOfJob(jobKey);
+                Date nextFireTime = triggers.get(0).getNextFireTime();
+                
+                System.out.println("[唯一标识] : " + jobName + " [群组] : "
+                        + jobGroup + " 下一次触发时间 " + SimpleDateFormat.getDateTimeInstance().format(nextFireTime));
+                
+            }
+            
         }
-        
-        
         
         
         Thread.sleep(60000);
---
+++ sample/src/test/java/in/hocg/sample/quartz/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @date 18-10-12
+ * @author hocgin
+ **/
+package in.hocg.sample.quartz;
