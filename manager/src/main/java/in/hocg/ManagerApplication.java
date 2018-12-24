57df357612bf9c8022dfbd2bb0202b92024602be
hocgin <hocgin@gmail.com>
2018-12-24 21:10:24 +0800

    + Redis

--- manager/src/main/java/in/hocg/ManagerApplication.java
+++ manager/src/main/java/in/hocg/ManagerApplication.java
@@ -2,12 +2,14 @@ package in.hocg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author hocgin
  * @date 18-10-10
  **/
 
+@EnableCaching
 @SpringBootApplication
 public class ManagerApplication {
     public static void main(String[] args) {
--- manager/src/main/java/in/hocg/manager/support/RedisCacheService.java
+++ manager/src/main/java/in/hocg/manager/support/RedisCacheService.java
@@ -1,6 +1,8 @@
 package in.hocg.manager.support;
 
 import in.hocg.scaffold.support.cache.CacheService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -13,24 +15,64 @@ import java.util.concurrent.TimeUnit;
  * @author hocgin
  */
 @Component
+@AllArgsConstructor
 public class RedisCacheService implements CacheService {
+    
+    private final RedisTemplate redisTemplate;
+    
     @Override
     public void set(Object key, Object value, long timeout, TimeUnit unit) {
-    
+        opsForValue().set(key, value, timeout, unit);
     }
     
     @Override
     public boolean contains(Object key) {
-        return false;
+        return redisTemplate.hasKey(key);
     }
     
     @Override
     public Optional<Object> get(Object key) {
-        return Optional.empty();
+        return Optional.ofNullable(opsForValue().get(key));
     }
     
     @Override
     public void delete(Object key) {
+        redisTemplate.delete(key);
+    }
     
+    /**
+     * 针对 K-V 结构
+     *
+     * @return
+     */
+    private ValueOperations opsForValue() {
+        return redisTemplate.opsForValue();
+    }
+    
+    /**
+     * 针对 Set 结构
+     *
+     * @return
+     */
+    private SetOperations opsForSet() {
+        return redisTemplate.opsForSet();
+    }
+    
+    /**
+     * 针对 List 结构
+     *
+     * @return
+     */
+    private ListOperations opsForList() {
+        return redisTemplate.opsForList();
+    }
+    
+    /**
+     * 针对 Map 结构
+     *
+     * @return
+     */
+    private HashOperations opsForHash() {
+        return redisTemplate.opsForHash();
     }
 }
